4b32d44fbf42c5d6f94f26b4fba968354a97197b
bobverity <r.verity@imperial.ac.uk>
2016-12-06 06:04:05 +0000

    first commit

---
+++ R/MAGENTA.R
@@ -0,0 +1,7 @@
+
+#' Package one-line descriptions
+#'
+#' Package short paragraph description
+#'
+#' Package further details
+"_PACKAGE"
---
+++ R/mainFunctions.R
@@ -0,0 +1,18 @@
+#------------------------------------------------
+#' Dummy function
+#'
+#' This is a dummy function
+#'
+#' @param x Some parameter
+#'
+#' @export
+#' @examples
+#' dummy1()
+
+# The following commands are needed to ensure that the roxygen2 package, which deals with documenting the package, does not conflict with the Rcpp package. Do not alter!
+#' @useDynLib MAGENTA
+#' @importFrom Rcpp evalCpp
+
+dummy1 <- function() {
+    print("dummy1")
+}
