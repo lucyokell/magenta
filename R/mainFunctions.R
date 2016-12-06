75aecbc3a99ea7fe49f9ba6980d32b378758ed32
bobverity <r.verity@imperial.ac.uk>
2016-12-06 06:16:57 +0000

    added exportPattern line

--- R/mainFunctions.R
+++ R/mainFunctions.R
@@ -12,6 +12,7 @@
 # The following commands are needed to ensure that the roxygen2 package, which deals with documenting the package, does not conflict with the Rcpp package. Do not alter!
 #' @useDynLib MAGENTA
 #' @importFrom Rcpp evalCpp
+#' @exportPattern "^[[:alpha:]]+"
 
 dummy1 <- function() {
     print("dummy1")
